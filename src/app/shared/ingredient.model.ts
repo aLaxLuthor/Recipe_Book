46cd4be290591d6ccef15b70e67ecedb576c9e19
Mathew Fraser <mathewfraser@gmail.com>
2017-11-24 09:22:52 +1100

    Added Shopping List & edit templates

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class ingredient{
+    //This will automatically create 2 public properties 
+    //based on the arguments passed in
+    constructor(public name: string, public amount: number){}
+}
