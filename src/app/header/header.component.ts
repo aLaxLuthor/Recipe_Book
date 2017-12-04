202480f9c69f32fe6f380c2b44e42dfaff944c76
Mathew Fraser <mathewfraser@gmail.com>
2017-12-04 21:37:27 +1100

    Updated recipe-item to component.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
 
+    onSelect(tabName: string){
+        this.featureSelected.emit(tabName);
+    }
 
 }
